2da2b3f5db8ae8a057b8502cb244b7950768fdef
Blue Space <gaoziyuan19@mails.ucas.ac.cn>
2023-07-22 01:27:47 +0800

    use busybox to run all tests to get complete busybox strace
    Former-commit-id: 2948b52971382a23c88605efe7d92523dd5a0ff1
    Former-commit-id: 4bb61a175425e10a4f420e8dd1659d5005a7b143

--- scripts/test_all.sh
+++ scripts/test_all.sh
@@ -5,32 +5,22 @@ mkdir -p /code/lmbench/riscv64/bin
 cp lmbench_all /code/lmbench/riscv64/bin
 
 busybox echo "run time-test"
-strace -o strace/time.txt ./time-test
-strace -c -o strace/time-list.txt ./time-test
+./time-test
 busybox echo "run busybox_testcode.sh"
-strace -o strace/busybox.txt ./busybox_testcode.sh
-strace -c -o strace/busybox-list.txt ./busybox_testcode.sh
+./busybox_testcode.sh
 busybox echo "run iozone_testcode.sh"
-strace -o strace/iozone.txt ./iozone_testcode.sh
-strace -c -o strace/iozone-list.txt ./iozone_testcode.sh
+./iozone_testcode.sh
 busybox echo "run libctest_testcode.sh"
-strace -o strace/libctest.txt ./libctest_testcode.sh
-strace -c -o strace/libctest-list.txt ./libctest_testcode.sh
+./libctest_testcode.sh
 busybox echo "run lmbench_testcode.sh"
-strace -o strace/lmbench.txt ./lmbench_testcode.sh
-strace -c -o strace/lmbench-list.txt ./lmbench_testcode.sh
+./lmbench_testcode.sh
 busybox echo "run lua_testcode.sh"
-strace -o strace/lua.txt ./lua_testcode.sh
-strace -c -o strace/lua-list.txt ./lua_testcode.sh
+./lua_testcode.sh
 busybox echo "run unixbench_testcode.sh"
-strace -o strace/unixbench.txt ./unixbench_testcode.sh
-strace -c -o strace/unixbench-list.txt ./unixbench_testcode.sh
+./unixbench_testcode.sh
 busybox echo "run netperf_testcode.sh"
-strace -o strace/netperf.txt ./netperf_testcode.sh
-strace -c -o strace/netperf-list.txt ./netperf_testcode.sh
+./netperf_testcode.sh
 busybox echo "run iperf_testcode.sh"
-strace -o strace/iperf.txt ./iperf_testcode.sh
-strace -c -o strace/iperf-list.txt ./iperf_testcode.sh
+./iperf_testcode.sh
 busybox echo "run cyclictest_testcode.sh"
-strace -o strace/cyclic.txt ./cyclictest_testcode.sh
-strace -c -o strace/cyclic-list.txt ./cyclictest_testcode.sh
+./cyclictest_testcode.sh
